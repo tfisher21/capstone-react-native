e771d3a2e46b49587a54abcd6521b8fde11fb73d
Tyler Fisher <tfisher21@gmail.com>
2018-12-06 18:16:41 -0600

    tweak button style

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import { Text, Input } from "react-native-elements";
+import { Button, Text, Input } from "react-native-elements";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import axios from "axios";
 
@@ -13,16 +13,27 @@ class LoginScreen extends Component {
     };
   }
 
-  render() {
-    function onPressLearnMore() {
-      console.log("Button Pressed");
-    }
+  loginUser() {}
 
+  render() {
     return (
       <View>
         <Text h4>Login to Actualize.Social</Text>
         <Input placeholder="email" />
         <Input placeholder="password" />
+        <Button
+          title="Login"
+          buttonStyle={{
+            marginTop: 20,
+            marginLeft: 300,
+            width: 70,
+            height: 45,
+            borderColor: "transparent",
+            borderWidth: 0,
+            borderRadius: 10
+          }}
+          onPress={this.loginUser}
+        />
       </View>
     );
   }
