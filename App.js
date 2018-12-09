5937179d3e665cd101cc5e94e4c562b6d5962f48
Tyler Fisher <tfisher21@gmail.com>
2018-12-09 12:03:55 -0600

    Implement Login capabilities.

--- App.js
+++ App.js
@@ -1,26 +1,57 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, AsyncStorage } from "react-native";
 import { Button, Text, Input } from "react-native-elements";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import axios from "axios";
+import Users from "./src/Users";
 
 class LoginScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      errors: ""
     };
+    this.loginUser = this.loginUser.bind(this);
   }
 
-  loginUser() {}
+  loginUser() {
+    var params = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    axios
+      .post("http://localhost:3000/api/sessions", params)
+      .then(response => {
+        axios.defaults.headers.common["Authorization"] =
+          "Bearer " + response.data.jwt;
+        AsyncStorage.setItem("jwt", response.data.jwt);
+        this.props.navigation.navigate("Details");
+      })
+      .catch(errors => {
+        console.log(errors.message);
+        this.setState({ errors: errors.message });
+      });
+  }
 
   render() {
     return (
       <View>
         <Text h4>Login to Actualize.Social</Text>
-        <Input placeholder="email" />
-        <Input placeholder="password" />
+        <Input
+          placeholder="email"
+          autoCapitalize="none"
+          onChangeText={email => this.setState({ email })}
+          valuse={this.state.email}
+        />
+        <Input
+          placeholder="password"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={password => this.setState({ password })}
+          valuse={this.state.password}
+        />
         <Button
           title="Login"
           buttonStyle={{
@@ -32,17 +63,32 @@ class LoginScreen extends Component {
             borderWidth: 0,
             borderRadius: 10
           }}
-          onPress={this.loginUser}
+          onPress={() => this.loginUser()}
         />
+        <Text> {this.state.errors} </Text>
       </View>
     );
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Login: {
-    screen: LoginScreen
+class DetailsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-});
+  render() {
+    return <Users />;
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Login"
+  }
+);
 
 export default createAppContainer(AppNavigator);
